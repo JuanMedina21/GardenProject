f940dfa399003764530617b8d8ce10c169d17e7d
Lars Conard <lars.conard@gmail.com>
2019-02-07 15:34:27 -0500

    NavBar, Footer, About, Contact

---
+++ src/app/about-co/about-co.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-co',
+  templateUrl: './about-co.component.html',
+  styleUrls: ['./about-co.component.css']
+})
+export class AboutCoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutCoComponent } from './about-co/about-co.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutCoComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
