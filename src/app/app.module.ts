9fd04e3690f11cc1d0a2072247b3e343844b2dcd
Lars Conard <lars.conard@gmail.com>
2019-02-07 16:25:16 -0500

    Final Home Page, includes Navbar and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { AboutCoComponent } from './about-co/about-co.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { FooterComponent } from './footer/footer.component';
     NavbarComponent,
     AboutCoComponent,
     ContactComponent,
-    FooterComponent
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
